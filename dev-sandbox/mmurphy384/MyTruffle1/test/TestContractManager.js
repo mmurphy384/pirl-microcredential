016ce50cc27da2468e5915556887526fbc4face3
Michael Murphy <mmurphy384@yahoo.com>
2018-04-18 21:06:39 -0400

    updated tests.  They work, now.

--- dev-sandbox/mmurphy384/MyTruffle1/test/TestContractManager.js
+++ dev-sandbox/mmurphy384/MyTruffle1/test/TestContractManager.js
@@ -1,18 +1,17 @@
 var ContractManager = artifacts.require("./ContractManager.sol");
 
 contract('ContractManager', function(accounts) {
-
+	var micro;
+	var account0 = "0x47ea1039993e27ce298246a7695dc6f7d6eeaacb";
 	it("should create 1 new contract", function() {
 		return ContractManager.deployed().then(function(instance) {
-			return Promise.all([
-				instance.newContract.call('ACME Testing Services1'),
-				instance.getContractCount.call()
-			]);
-		}).then(function (results) {
-			console.log('############### Log Entry: Contract Addres = ' + results[0].valueOf());
-			console.log('############### Log Entry: Num Contracts= ' + results[1].valueOf());
-			assert.equal(results[0].valueOf().length, 42, "The contract address is valid");
-			assert.equal(results[1].toNumber(), 1, "The count of contracts should be 1");
+			micro = ContractManager.at(instance.address);
+			return micro.newContract('ACME Testing Services1');
+		}).then(function (result) {
+			assert.isBelow(result.receipt.gasUsed,200000,'Gas did not exceed 200000');
+			return micro.getContractCount.call();
+		}).then(function (result) {
+			assert.equal(result.toNumber(), 1, "The count of contracts should be 1");
 		});
 	});
 })

07cee672023adab987ca30ef32a136bfaa9d1277
Michael Murphy <mmurphy384@yahoo.com>
2018-05-05 20:51:12 -0400

    Removed sandbox folders

--- dev-sandbox/mmurphy384/MyTruffle1/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var ContractManager = artifacts.require("./ContractManager.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ContractManager);
-};
--- dev-sandbox/mmurphy384/MyTruffle2/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var Factory = artifacts.require("./Factory.sol");
-var App = artifacts.require("./Factory.sol");
-
-
-module.exports = function(deployer) {
-  deployer.deploy(Factory);
-  deployer.link(Factory, App);
-  deployer.deploy(App);
-};

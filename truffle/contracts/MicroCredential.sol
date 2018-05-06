6d2731a557645e0c52f2e3b85bb4b5e35015001b
Michael Murphy <mmurphy384@yahoo.com>
2018-05-06 10:27:02 -0400

    Moved Agency struct to variables

--- truffle/contracts/MicroCredential.sol
+++ truffle/contracts/MicroCredential.sol
@@ -5,15 +5,11 @@ import "./ownable.sol";
 contract MicroCredential is Ownable {
 
     // variables and such
-    Agency private agency;
-
-    struct Agency {
-        bytes32 name;
-        bytes32 website;
-        bytes32 email;
-        int perReviewFeeInPirl;
-        bool isActive;
-    }
+    bytes32 agencyName;
+    bytes32 website;
+    bytes32 email;
+    int perReviewFeeInPirl;
+    bool isActive;
 
     // Events
     event Deposit(address _from, uint value);
@@ -30,35 +26,35 @@ contract MicroCredential is Ownable {
     }
 
     // Purpose  : Set the agency basic info
-    function setAgencyInfo(bytes32 _name, bytes32 _website, bytes32 _email, int _perReviewFeeInPirl) 
+    function setAgencyInfo(bytes32 _agencyName, bytes32 _website, bytes32 _email, int _perReviewFeeInPirl) 
         public onlyOwner {
-        agency.name = _name;
-        agency.website = _website;
-        agency.email = _email;
-        agency.perReviewFeeInPirl = _perReviewFeeInPirl;
-        agency.isActive = true;
+        agencyName = _agencyName;
+        website = _website;
+        email = _email;
+        perReviewFeeInPirl = _perReviewFeeInPirl;
+        isActive = true;
     }
 
     // Purpose  : Get the agency basic info
     function getAgencyInfo() view public returns (bytes32, bytes32, bytes32, int, bool) {
-        return (agency.name, agency.website, agency.email, agency.perReviewFeeInPirl, agency.isActive);
+        return (agencyName, website, email, perReviewFeeInPirl, isActive);
     }
 
     // Purpose  : To Make the contract inactive
     // To Do    : In the future, this might do additional things
     //            such as return fees, clear statuses, etc.
     function setAgencyInactive() public onlyOwner {
-        agency.isActive = false;
+        isActive = false;
     }
 
     // Purpose  : Used when an agency becomes active again
     // To Do    : To be determined
     function setAgencyActive() public onlyOwner {
-        agency.isActive = false;
+        isActive = false;
     }
 
     // Purpose  : Get the contract balance
-    function getContractBalance() public view onlyOwner returns (uint) {
+    function getContractBalance() public view returns (uint) {
         return address(this).balance;
     }
 
